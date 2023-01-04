18d2a56a8396248555825135573dcf065e55d738
Gabriel Henrique <gabriel.ferreira@ar.com.br>
2023-01-04 16:10:24 -0300

    ajustes

---
+++ src/components/Card/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Container,Label } from "./styles";
+
+export default function List(){
+    return(
+        <Container>
+            <header>
+                <Label color="#7159C1"/>
+            </header>
+            <p>Fazer as tarefas de React</p>
+        </Container>
+    );
+}
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import { MdAdd } from 'react-icons/md';
+import Card from '../Card';
+
 import { Container } from "./styles";
 
 export default function List(){
@@ -12,6 +14,13 @@ export default function List(){
                 <MdAdd size={24} color="#fff" />
             </button>
            </header>
+           <ul>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+           </ul>
         </Container>
     );
 }
